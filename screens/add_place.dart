26fa1bc42562d9a533becbe4089e3d351675ee41
Nie <crystalny03@gmail.com>
2024-01-27 18:48:24 -0500

    add image picker
    allow user to take a picture and display.
    (for ios system, remember to add keys to ios/Runner/info.plist

--- screens/add_place.dart
+++ screens/add_place.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:favorite_places/providers/user_places.dart';
+import 'package:favorite_places/widgets/image_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -13,15 +16,16 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
+  File? _selectedImage;
 
   void _savePlace() {
     final enteredTitle = _titleController.text; // textController ensures to recieve something even if an empty string, so never null
-
-    if (enteredTitle.isEmpty) {
+  
+    if (enteredTitle.isEmpty || _selectedImage == null) {
       return;
     }
 
-    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle);
+    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle, _selectedImage!);
 
     Navigator.of(context).pop();
   }
@@ -49,6 +53,14 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 color: Theme.of(context).colorScheme.onBackground,
               ),
             ),
+
+            // image input
+
+            ImageInput(onPickImage: (image) {
+              _selectedImage = image;
+            },),
+            const SizedBox(height: 16),
+            const Text('(Tap image to retake picture.)'),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
