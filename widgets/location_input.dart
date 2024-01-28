ee71a475aa85dc3f401dffb5626f24bdaa5a29e6
Nie <crystalny03@gmail.com>
2024-01-27 19:25:29 -0500

    add get lcoation
    use location package to get location in add_place

---
+++ widgets/location_input.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({super.key});
+
+  @override
+  State<LocationInput> createState() {
+    return _LocationInputState();
+  }
+}
+
+class _LocationInputState extends State<LocationInput> {
+  var _isGettingLocation = false;
+
+  void _getCurrentLocation() async {
+    Location location = Location();
+
+    // some checks from getting the location
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    // change state and start to get location
+    setState(() {
+      _isGettingLocation = true;
+    });
+
+    LocationData locationData;
+    locationData = await location.getLocation();
+
+    // set back to false once we are done getting location
+    setState(() {
+      _isGettingLocation = false;
+    });
+
+    locationData.latitude;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget previewContent = Text(
+      'No location chosen',
+      textAlign: TextAlign.center,
+      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+            color: Theme.of(context).colorScheme.onBackground,
+          ),
+    );
+
+    // to show a loading spinner
+    if (_isGettingLocation) {
+      previewContent = const CircularProgressIndicator();
+    }
+
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+            ),
+          ),
+          child: previewContent,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton.icon(
+              icon: const Icon(Icons.location_on),
+              label: const Text('Get Current Location'),
+              onPressed: _getCurrentLocation,
+            ),
+            TextButton.icon(
+              icon: const Icon(Icons.map),
+              label: const Text('Select on Map'),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
